5c50e0ec292553eaa54524081a6f503e0b5357f2
touchmeangel <vodkavoid@proton.me>
2025-10-31 20:04:38 +0200

    +browser imitation

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ workdir = os.path.join(os.getcwd(), "sessions")
 os.makedirs(workdir, exist_ok=True)
 init(autoreset=True)
 
-app = Client("session", device_model="Gift Snipper @touchmeh", client_platform=ClientPlatform.ANDROID, app_version="Android 11.14.1", api_id=api_id, api_hash=api_hash, workdir=workdir)
+app = Client("session", device_model="Chrome 141 @touchmeh", client_platform=ClientPlatform.WEB, api_id=api_id, api_hash=api_hash, workdir=workdir)
 logger = logging.getLogger(__name__)
 tg_logger = TGLogger(logger_token, logger_chat_id)
 async def main():
